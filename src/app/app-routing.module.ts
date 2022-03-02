078d2b4f21f269a2b50a7b45e2b578303b4bcc99
dannydjg16 <dannydjg16@gmail.com>
2022-03-02 15:47:26 -0500

    added to user service, and routing module
    also added the baseUrl to the environment file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { UserComponent } from './components/user/user.component';
+import { Routes, RouterModule } from '@angular/router';
 
 
+const routes: Routes = [
+  { path: 'users', component: UserComponent },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import  User  from '../interfaces/user';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private baseUrl = `${environment.baseUrl}/users`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]>{
+    const headers = {
+      Accept: 'application/json'
+    };
+    return this.http.get<User[]>(`${this.baseUrl}`, { headers: headers })
+  }
 }

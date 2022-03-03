d05a0ac64f1cfd5f872d178ac78e08de0eb1b42a
dannydjg16 <dannydjg16@gmail.com>
2022-03-03 15:58:37 -0500

    Beginning Adding okta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { OktaAuthModule, OKTA_CONFIG } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+
+const oktaAuth = new OktaAuth({
+  issuer: 'https://dev-7824301.okta.com/oauth2/default',
+  clientId: '0oa41j4h92rnKJ2Xr5d7',
+  redirectUri: window.location.origin + '/login/callback'
+});
+
 
 @NgModule({
   declarations: [
@@ -14,7 +23,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    OktaAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]

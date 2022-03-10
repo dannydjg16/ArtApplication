44f874b834be5578b8265e1a999a3fe04aa96703
dannydjg16 <dannydjg16@gmail.com>
2022-03-10 01:39:15 -0500

    get db user from okta signed in user

--- src/app/components/okta-user/okta-user.component.ts
+++ src/app/components/okta-user/okta-user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { OktaAuthStateService } from '@okta/okta-angular';
-import { filter, map, Observable, Subscription } from 'rxjs';
+import { filter, map, Observable, of, Subscription } from 'rxjs';
 import { AuthState, OktaAuth } from '@okta/okta-auth-js';
 import { OKTA_AUTH } from '@okta/okta-angular';
 import { OktaAuthModule, OktaCallbackComponent, OKTA_CONFIG } from '@okta/okta-angular';
@@ -16,42 +16,24 @@ import User from 'src/app/interfaces/user';
 })
 export class OktaUserComponent implements OnInit {
 
-  public name$!:Observable<string>;
+  public name$!: Observable<string>;
   public isAuthenticated$!: Observable<boolean>;
   public fullName$!: string;
-  public user$!: User; 
+  public user$!: User;
 
   constructor(private _router: Router, private _oktaStateService: OktaAuthStateService, private userService: UserService) { }
 
   public ngOnInit(): void {
-    // this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
-    //   filter((s: AuthState) => !!s),
-    //   map((s: AuthState) => s.isAuthenticated ?? false),
-    // );
+    this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
+      filter((s: AuthState) => !!s),
+      map((s: AuthState) => s.isAuthenticated ?? false),
+    );
 
-
-    //this.name$ = this._oktaStateService.authState$.subscribe(as => this.userService.getUserByEmail(as.accessToken?.claims.sub!));
-    
-    // this.name$ = this._oktaStateService.authState$.pipe(
-    //   // map((authState: AuthState) => {
-    //   //   this.userService.getUserByEmail(authState.accessToken?.claims.sub!).subscribe(n => n.name)
-    //   // }).toString()
-    //   a => 
-    // );
+    this._oktaStateService.authState$.subscribe(as => this.userService.getUserByEmail(as.accessToken?.claims.sub!).subscribe(u => this.fullName$ = u.name));
 
     this.name$ = this._oktaStateService.authState$.pipe(
       filter((authState: AuthState) => !!authState && !!authState.isAuthenticated),
       map((authState: AuthState) => authState.accessToken?.claims.sub ?? 'Okta-User-Component')
     );
-
-
-    // this.name$ = this._oktaStateService.authState$.pipe(
-    //   filter((authState: AuthState) => !!authState && !!authState.isAuthenticated),
-    //   map((authState: AuthState) => this.userService.getUserByEmail(authState.accessToken?.claims.sub!).subscribe(n => n.name!))
-    // );
-
-    // if (this.name$) {
-    //   this.user$ = this.userService.getUserByEmail(this.name$).subscribe;
-    // }
   }
 }

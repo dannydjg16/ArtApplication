e9cc0bab9a66c229a61794d6df3b29b7c7d153d4
dannydjg16 <dannydjg16@gmail.com>
2022-03-11 15:53:40 -0500

    save before adding MDBootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { OktaUserComponent } from './components/okta-user/okta-user.component';
 import { NoAuthNavComponent } from './components/no-auth-nav/no-auth-nav.component';
 import { AuthNavComponent } from './components/auth-nav/auth-nav.component';
 
+
 const oktaAuth = new OktaAuth({
   issuer: 'https://dev-7824301.okta.com/oauth2/default',
   clientId: '0oa41j4h92rnKJ2Xr5d7',

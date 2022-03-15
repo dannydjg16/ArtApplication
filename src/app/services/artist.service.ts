e779d37c623d9f21b3c71aa92742b58905e166fc
dannydjg16 <dannydjg16@gmail.com>
2022-03-14 22:39:46 -0400

    getArtists function on ArtistService

--- src/app/services/artist.service.ts
+++ src/app/services/artist.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import Artist from '../interfaces/artist';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtistService {
+  private baseUrl = `${environment.baseUrl}/artists`;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getArtists(): Observable<Artist[]> {
+    const headers = {
+      Accept: 'application/json'
+    };
+    return this.http.get<Artist[]>(`${this.baseUrl}`, { headers: headers })
+  }
+
+  
 }

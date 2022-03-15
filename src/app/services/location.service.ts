f743c7daa86562a2e0ffd8502d1720d7a5c877b2
dannydjg16 <dannydjg16@gmail.com>
2022-03-15 07:39:43 -0400

    Location Service

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,9 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
+  
+  private baseUrl = `${environment.baseUrl}/locations`;
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getLocations(name?: string): Observable<Location[]> {
+    const headers = {
+      Accept: 'application/json',
+    };
+
+    return this.http.get<Location[]>(`${this.baseUrl}`, { headers: headers })
+  }
+
+  getLocationById(id: number): Observable<Location> {
+    const headers = {
+      Accept: 'application/json',
+    };
+
+    return this.http.get<Location>(`${this.baseUrl}/${id}`, { headers: headers })
+  }
+
+  addLocation(location: Location) {
+    const headers = {
+      Accept: 'application/json',
+    };
+
+    return this.http.post(`${this.baseUrl}`, location, { headers: headers});
+  }
+
+  editLocation(id: number, location: Location) {
+    const headers = {
+      Accept: 'application/json',
+    }
+
+    return this.http.put(`${this.baseUrl}/${id}`, location, { headers: headers});
+  }
 }

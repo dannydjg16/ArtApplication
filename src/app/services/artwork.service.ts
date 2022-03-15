5cc7648da162c8c57cb64c2481a461ea18a76453
dannydjg16 <dannydjg16@gmail.com>
2022-03-15 07:30:05 -0400

    Artwork Service

--- src/app/services/artwork.service.ts
+++ src/app/services/artwork.service.ts
@@ -1,9 +1,55 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import Artwork from '../interfaces/artwork';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtworkService {
+  
+  private baseUrl = `${environment.baseUrl}/artwork`;
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getArtworks(title?: string): Observable<Artwork[]> {
+    const headers = {
+      Accept: 'application/json',
+    };
+
+    return this.http.get<Artwork[]>(`${this.baseUrl}`, { headers: headers })
+  }
+
+  getArtorkById(id: number): Observable<Artwork> {
+    const headers = {
+      Accept: 'application/json',
+    };
+
+    return this.http.get<Artwork>(`${this.baseUrl}/${id}`, { headers: headers })
+  }
+
+  addArtwork(artWork: Artwork) {
+    const headers = {
+      Accept: 'application/json',
+    };
+
+    return this.http.post(`${this.baseUrl}`, artWork, { headers: headers});
+  } 
+
+  editArtwork(id: number, artWork: Artwork) {
+    const headers = {
+      Accept: 'application/json',
+    };
+
+    return this.http.put(`${this.baseUrl}/${id}`, artWork, { headers: headers});
+  } 
+
+  deleteArtwork(id: number) {
+    const headers = {
+      Accept: 'application/json',
+    };
+
+    return this.http.delete(`${this.baseUrl}/${id}`, { headers: headers});
+  }
 }

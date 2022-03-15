fcf5cbf6b54a2f204057c727e8d6b3869dc17b23
dannydjg16 <dannydjg16@gmail.com>
2022-03-15 07:44:00 -0400

    LocationTypeService

--- src/app/services/locationtype.service.ts
+++ src/app/services/locationtype.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import LocationType from '../interfaces/locationtype';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationTypeService {
 
-  constructor() { }
+  private baseUrl = `${environment.baseUrl}/locationtype`;
+
+  constructor(private http: HttpClient) { }
+
+  getLocationTypes(): Observable<LocationType[]> {
+    const headers = {
+      Accept: 'application/json',
+    }
+
+    return this.http.get<LocationType[]>(`${this.baseUrl}`, { headers: headers});
+  }
+
+  addLocationType(locationType: LocationType) {
+    const headers = {
+      Accept: 'application/json',
+    };
+
+    return this.http.post(`${this.baseUrl}`, locationType, { headers: headers});
+  }
 }

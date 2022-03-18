b0d3d9f8c22dd338c5fa31d282d43e1b4f4ea8a1
dannydjg16 <dannydjg16@gmail.com>
2022-03-18 15:59:26 -0400

    Update User Component for auth

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import User from 'src/app/interfaces/user';
+import { filter, map, Observable } from 'rxjs';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
+import { AuthState, OktaAuth } from '@okta/okta-auth-js';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -9,12 +13,19 @@ import User from 'src/app/interfaces/user';
 })
 export class UserComponent implements OnInit {
 
+  public isAuthenticated$!: Observable<boolean>;
+
   users: User[] | null = null;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private _router: Router, private _oktaStateService: OktaAuthStateService, @Inject(OKTA_AUTH) private _oktaAuth: OktaAuth) { }
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe(users => this.users = users);
+
+    this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
+      filter((s: AuthState) => !!s),
+      map((s: AuthState) => s.isAuthenticated ?? false)
+    );
   }
 
   

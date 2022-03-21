fdd8863106399a94433eb6c89b757631777821a1
dannydjg16 <dannydjg16@gmail.com>
2022-03-20 22:12:55 -0400

    Added isAuthenticated to two files

--- src/app/components/user-gallery/user-gallery.component.ts
+++ src/app/components/user-gallery/user-gallery.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import { AuthState } from '@okta/okta-auth-js';
+import { filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-gallery',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserGalleryComponent implements OnInit {
 
-  constructor() { }
+  public isAuthenticated$!: Observable<boolean>;
+
+  constructor(private _oktaStateService: OktaAuthStateService) { }
 
   ngOnInit(): void {
+    this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
+      filter((s: AuthState) => !!s),
+      map((s: AuthState) => s.isAuthenticated ?? false)
+    );
   }
 
 }

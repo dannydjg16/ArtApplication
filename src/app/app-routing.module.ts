05b2bed4ab3135a94297f29dfb3e87d5ce1cc478
dannydjg16 <dannydjg16@gmail.com>
2022-03-22 13:20:01 -0400

    Footer positioned

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { UserComponent } from './components/user/user.component';
 import { Routes, RouterModule } from '@angular/router';
 import { OktaCallbackComponent } from '@okta/okta-angular';
 import { AddArtComponent } from './components/add-art/add-art.component';
+import { GeneralGalleryComponent } from './components/general-gallery/general-gallery.component';
 
 
 const routes: Routes = [
   { path: 'users', component: UserComponent },
   { path: 'login/callback', component: OktaCallbackComponent },
   { path: 'addwork', component: AddArtComponent},
+  { path: 'gallery', component: GeneralGalleryComponent}
 ];
 
 @NgModule({

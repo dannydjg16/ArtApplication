5d440ab4712a62eb878cd2ec857b24a76a7de6e3
dannydjg16 <dannydjg16@gmail.com>
2022-03-21 22:51:04 -0400

    Got artworks into front end
    Not viewing them yet but that is the next step

--- src/app/components/general-gallery/general-gallery.component.ts
+++ src/app/components/general-gallery/general-gallery.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { OktaAuthStateService } from '@okta/okta-angular';
 import { AuthState } from '@okta/okta-auth-js';
 import { filter, map, Observable } from 'rxjs';
+import Artwork from 'src/app/interfaces/artwork';
+import { ArtworkService } from 'src/app/services/artwork.service';
 
 @Component({
   selector: 'app-general-gallery',
@@ -11,14 +13,21 @@ import { filter, map, Observable } from 'rxjs';
 export class GeneralGalleryComponent implements OnInit {
 
   public isAuthenticated$!: Observable<boolean>;
+  public artworks$!: Observable<Artwork[]>
+  public artworkss!: Artwork[];
 
-  constructor(private _oktaStateService: OktaAuthStateService) { }
+  constructor(private _oktaStateService: OktaAuthStateService, private _artworkService: ArtworkService) { }
 
   ngOnInit(): void {
     this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
       filter((s: AuthState) => !!s),
       map((s: AuthState) => s.isAuthenticated ?? false)
     );
+  
+    this._artworkService.getArtworks().subscribe(aws => this.artworkss = aws);
   }
+
+
+
 
 }

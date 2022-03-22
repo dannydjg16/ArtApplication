56f3539b23d9106b35ddf3c36275aed7c456a529
dannydjg16 <dannydjg16@gmail.com>
2022-03-22 12:56:59 -0400

    various

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { UserComponent } from './components/user/user.component';
 import { Routes, RouterModule } from '@angular/router';
 import { OktaCallbackComponent } from '@okta/okta-angular';
+import { AddArtComponent } from './components/add-art/add-art.component';
 
 
 const routes: Routes = [
   { path: 'users', component: UserComponent },
-  { path: 'login/callback', component: OktaCallbackComponent }
+  { path: 'login/callback', component: OktaCallbackComponent },
+  { path: 'addwork', component: AddArtComponent},
 ];
 
 @NgModule({

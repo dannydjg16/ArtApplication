9a5cc4f6d1bbb07271b0ca5f328cce2bc469adf1
dannydjg16 <dannydjg16@gmail.com>
2022-03-22 00:17:10 -0400

    Sign in Button moved to navbar

--- src/app/components/no-auth-nav/no-auth-nav.component.ts
+++ src/app/components/no-auth-nav/no-auth-nav.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 
 @Component({
   selector: 'app-no-auth-nav',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoAuthNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router, @Inject(OKTA_AUTH) private _oktaAuth: OktaAuth) { }
 
   ngOnInit(): void {
   }
 
+  public async signIn() : Promise<void> {
+    await this._oktaAuth.signInWithRedirect().then(
+      _ => this._router.navigate(['/profile'])
+    );
+  }
 }

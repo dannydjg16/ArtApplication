75d682a739d80ce86e968d26ef29ed96953dc393
dannydjg16 <dannydjg16@gmail.com>
2022-03-23 13:22:31 -0400

    Sent artwork to gallery card component

--- src/app/components/gallery-card/gallery-card.component.ts
+++ src/app/components/gallery-card/gallery-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gallery-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery-card.component.css']
 })
 export class GalleryCardComponent implements OnInit {
+
+  @Input() theArtwork : any;
 
   constructor() { }
 

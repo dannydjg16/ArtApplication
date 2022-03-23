f1d925e869b63f4493528108b2539b6c560a3cf5
dannydjg16 <dannydjg16@gmail.com>
2022-03-22 22:52:18 -0400

    Add art and footer

--- src/app/components/add-art/add-art.component.ts
+++ src/app/components/add-art/add-art.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ArtworkService } from 'src/app/services/artwork.service';
+import  Artwork  from '../../interfaces/artwork';
 
 @Component({
   selector: 'app-add-art',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddArtComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _artworkService: ArtworkService) { }
 
   ngOnInit(): void {
+  }
+
+
+  add(title: string, url: string, year: string, description: string, artist: string, medium: string, location: string) {
+    const artwork = {
+      title: title, filename: url, yearCreated: Number(year), description: description, locationNow: Number(location), artistID: Number(artist), mediumID: Number(medium)
+    }
+
+    this._artworkService.addArtwork(artwork as unknown as Artwork).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
--- src/app/services/artwork.service.ts
+++ src/app/services/artwork.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import Artwork from '../interfaces/artwork';
 
@@ -34,7 +34,11 @@ export class ArtworkService {
       Accept: 'application/json',
     };
 
-    return this.http.post(`${this.baseUrl}`, artWork, { headers: headers});
+    return this.http.post(`${this.baseUrl}`, artWork, { headers: headers})
+    // .pipe(catchError((err) => {
+    //   console.error(err);
+    //   throw err;
+    // }));;
   } 
 
   editArtwork(id: number, artWork: Artwork) {
@@ -42,7 +46,7 @@ export class ArtworkService {
       Accept: 'application/json',
     };
 
-    return this.http.put(`${this.baseUrl}/${id}`, artWork, { headers: headers});
+    return this.http.put(`${this.baseUrl}/${id}`, artWork, { headers: headers})
   } 
 
   deleteArtwork(id: number) {

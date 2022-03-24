ac5ec4181ffab97415983876796cd5d24b561753
dannydjg16 <dannydjg16@gmail.com>
2022-03-24 16:40:37 -0400

    Add Artist Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { OktaCallbackComponent } from '@okta/okta-angular';
 import { AddArtComponent } from './components/add-art/add-art.component';
 import { GeneralGalleryComponent } from './components/general-gallery/general-gallery.component';
+import { AddArtistComponent } from './components/add-artist/add-artist.component';
 
 
 const routes: Routes = [
   { path: 'users', component: UserComponent },
   { path: 'login/callback', component: OktaCallbackComponent },
-  { path: 'addwork', component: AddArtComponent},
-  { path: 'gallery', component: GeneralGalleryComponent}
+  { path: 'addwork', component: AddArtComponent },
+  { path: 'gallery', component: GeneralGalleryComponent },
+  { path: 'addartist', component: AddArtistComponent },
 ];
 
 @NgModule({

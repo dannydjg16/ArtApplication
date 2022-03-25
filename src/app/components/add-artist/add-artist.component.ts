1b9c147add426be5b6ccaa3f4641302be151a1e1
dannydjg16 <dannydjg16@gmail.com>
2022-03-25 18:50:02 -0400

    AddArtist method

--- src/app/components/add-artist/add-artist.component.ts
+++ src/app/components/add-artist/add-artist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Artist from 'src/app/interfaces/artist';
+import { ArtistService } from 'src/app/services/artist.service';
 
 @Component({
   selector: 'app-add-artist',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddArtistComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _artistService: ArtistService) { }
 
   ngOnInit(): void {
+  }
+
+
+  add(name: string, url: string, bornLocation: string, born: string, died: string, biography: string) {
+    const artist = {
+      name: name, pictureURL: url, bornLocation: bornLocation, born: born, died: died, biography: biography
+    }
+
+    this._artistService.addArtist(artist as unknown as Artist).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }

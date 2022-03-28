c5c0f7f349ef51a0850a46dbed8fa0921e076ce7
dannydjg16 <dannydjg16@gmail.com>
2022-03-28 16:56:20 -0400

    test yml
    also added the add function for artType

--- src/app/components/art-type/art-type.component.ts
+++ src/app/components/art-type/art-type.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import ArtType from 'src/app/interfaces/arttype';
+import { ArttypeService } from 'src/app/services/arttype.service';
 
 @Component({
   selector: 'app-art-type',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtTypeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _artTypeService: ArttypeService) { }
 
   ngOnInit(): void {
   }
 
+
+  add(title: string, description: string) {
+    const artType = {
+      title: title, description: description
+    }
+
+    this._artTypeService.addArtType(artType as unknown as ArtType).subscribe(data => {
+      console.log(data);
+    });
+  }
 }

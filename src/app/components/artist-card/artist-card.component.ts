cc79981057e3e61976e9f6e28d42a782c8dad2e0
dannydjg16 <dannydjg16@gmail.com>
2022-03-31 15:58:15 -0400

    Artist Gallery coming along except no picture

--- src/app/components/artist-card/artist-card.component.ts
+++ src/app/components/artist-card/artist-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-artist-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist-card.component.css']
 })
 export class ArtistCardComponent implements OnInit {
+
+  @Input() theArtist : any;
 
   constructor() { }
 

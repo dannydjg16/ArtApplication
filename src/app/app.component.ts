3f4f4bfc9299fb5f4c94f648d1f2fe09108d06fe
dannydjg16 <dannydjg16@gmail.com>
2022-03-30 20:12:57 -0400

    Fixed A couple of errors
    POST user and GET user that were occurring when nothing even was happening

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,11 +16,16 @@ export class AppComponent implements OnInit {
   title = 'DGArt';
   public isAuthenticated$!: Observable<boolean>;
   public fullName$!: string;
+  isAuthenticated = false;
 
   constructor(private _router: Router, private _oktaStateService: OktaAuthStateService, @Inject(OKTA_AUTH) private _oktaAuth: OktaAuth, private userService: UserService) {
     this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
       filter((s: AuthState) => !!s),
       map((s: AuthState) => s.isAuthenticated ?? false)
+    );
+
+    this._oktaStateService.authState$.subscribe(
+      s => this.isAuthenticated = s.isAuthenticated!
     );
    }
 
@@ -31,11 +36,15 @@ export class AppComponent implements OnInit {
     );
 
     this._oktaStateService.authState$.subscribe(as => {
-      this.userService.getUserByEmail(as.accessToken?.claims.sub!).subscribe(
-        u => this.fullName$ = u.name,
-        err => this.userService.addUser({ id: 0, email: as.accessToken?.claims.sub!, name: as.accessToken?.claims.name!, fromLocation: ''}).subscribe())
+      if (this.isAuthenticated) {
+        this.userService.getUserByEmail(as.accessToken?.claims.sub!).subscribe(
+          u => this.fullName$ = u.name,
+          err => this.userService.addUser({ id: 0, email: as.accessToken?.claims.sub!, name: as.accessToken?.claims.name!, fromLocation: ''}).subscribe(
+            () => this._router.navigate(['/']),
+            () => this._router.navigate(['/'])
+          ))
+      }
     });
-      
   }
 
   public async signIn() : Promise<void> {

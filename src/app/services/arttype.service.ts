a97b58f42eba41e1f931bd14a87e8d71627f4e30
dannydjg16 <dannydjg16@gmail.com>
2022-04-05 00:17:22 -0400

    ArtType Service Auth

--- src/app/services/arttype.service.ts
+++ src/app/services/arttype.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { OktaAuth } from '@okta/okta-auth-js';
 import { catchError, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import ArtType from '../interfaces/arttype';
@@ -11,10 +12,12 @@ export class ArttypeService {
 
   private baseUrl = `${environment.baseUrl}/arttype`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _oktaAuth: OktaAuth) { }
 
   getArtTypes(): Observable<ArtType[]> {
+    const accessToken = this._oktaAuth.getAccessToken();
     const headers = {
+      Authorization: 'Bearer ' + accessToken,
       Accept: 'application/json',
     };
     return this.http.get<ArtType[]>(`${this.baseUrl}`, { headers: headers }).pipe(catchError((err) => {
@@ -24,7 +27,9 @@ export class ArttypeService {
   }
 
   addArtType(arttype: ArtType) {
+    const accessToken = this._oktaAuth.getAccessToken();
     const headers = {
+      Authorization: 'Bearer ' + accessToken,
       Accept: 'application/json'
     };
     return this.http.post(`${this.baseUrl}`, arttype, {headers: headers}).pipe(catchError((err) => {

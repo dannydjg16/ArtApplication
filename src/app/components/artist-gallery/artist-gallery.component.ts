f1a90c4ceed125315549c53e56b6852f9c11a59f
dannydjg16 <dannydjg16@gmail.com>
2022-04-08 14:15:38 -0400

    Artist Gallery Only Shows on Auth

--- src/app/components/artist-gallery/artist-gallery.component.ts
+++ src/app/components/artist-gallery/artist-gallery.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OktaAuthStateService } from '@okta/okta-angular';
+import { AuthState } from '@okta/okta-auth-js';
+import { filter, map, Observable } from 'rxjs';
 import Artist from 'src/app/interfaces/artist';
 import { ArtistService } from 'src/app/services/artist.service';
 
@@ -10,11 +12,16 @@ import { ArtistService } from 'src/app/services/artist.service';
 })
 export class ArtistGalleryComponent implements OnInit {
 
+  public isAuthenticated$!: Observable<boolean>;
   public artists!: Artist[];
 
   constructor(private _oktaStateService: OktaAuthStateService, private _artistService: ArtistService) { }
 
   ngOnInit(): void {
+    this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
+      filter((s: AuthState) => !!s),
+      map((s: AuthState) => s.isAuthenticated ?? false)
+    );
 
     this._artistService.getArtists().subscribe(arts => this.artists = arts);
   }

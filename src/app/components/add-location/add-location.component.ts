e1c5004d980490c172b22752575c37124812fa51
dannydjg16 <dannydjg16@gmail.com>
2022-04-08 14:10:06 -0400

    Add Location only shows on auth

--- src/app/components/add-location/add-location.component.ts
+++ src/app/components/add-location/add-location.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import { AuthState } from '@okta/okta-auth-js';
+import { filter, map, Observable } from 'rxjs';
 import { LocationService } from 'src/app/services/location.service';
 
 @Component({
@@ -8,9 +11,15 @@ import { LocationService } from 'src/app/services/location.service';
 })
 export class AddLocationComponent implements OnInit {
 
-  constructor(private _locationService: LocationService) { }
+  public isAuthenticated$!: Observable<boolean>;
+
+  constructor(private _oktaStateService: OktaAuthStateService, private _locationService: LocationService) { }
 
   ngOnInit(): void {
+    this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
+      filter((s: AuthState) => !!s),
+      map((s: AuthState) => s.isAuthenticated ?? false)
+    );
   }
 
   add(locationName: string, locationURL: string, description: string, locationTypeID: string) {

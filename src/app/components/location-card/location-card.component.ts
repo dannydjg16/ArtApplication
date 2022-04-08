1665d552d44fa85ca99b45f998a8301aeff0adeb
dannydjg16 <dannydjg16@gmail.com>
2022-04-07 23:56:36 -0400

    Location Gallery additions

--- src/app/components/location-card/location-card.component.ts
+++ src/app/components/location-card/location-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-location-card',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationCardComponent implements OnInit {
 
+  @Input() theLocation : any;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+

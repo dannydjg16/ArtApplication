8040871cdea6953be9f2d6db754a8ebe0c90cb9b
dannydjg16 <dannydjg16@gmail.com>
2022-04-08 13:56:57 -0400

    Add Artist only shows on auth

--- src/app/components/add-artist/add-artist.component.ts
+++ src/app/components/add-artist/add-artist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import { AuthState } from '@okta/okta-auth-js';
+import { filter, map, Observable } from 'rxjs';
 import Artist from 'src/app/interfaces/artist';
 import { ArtistService } from 'src/app/services/artist.service';
 
@@ -9,9 +12,16 @@ import { ArtistService } from 'src/app/services/artist.service';
 })
 export class AddArtistComponent implements OnInit {
 
-  constructor(private _artistService: ArtistService) { }
+  public isAuthenticated$!: Observable<boolean>;
+
+
+  constructor(private _oktaStateService: OktaAuthStateService, private _artistService: ArtistService) { }
 
   ngOnInit(): void {
+    this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
+      filter((s: AuthState) => !!s),
+      map((s: AuthState) => s.isAuthenticated ?? false)
+    );
   }
 
 

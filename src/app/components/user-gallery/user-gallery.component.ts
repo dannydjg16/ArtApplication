825e62bcd18dd1f0bd76eca9c75921405a58f18e
dannydjg16 <dannydjg16@gmail.com>
2022-04-12 01:12:47 -0400

    Method to get Users liked posts

--- src/app/components/user-gallery/user-gallery.component.ts
+++ src/app/components/user-gallery/user-gallery.component.ts
@@ -3,6 +3,8 @@ import { OktaAuthStateService } from '@okta/okta-angular';
 import { AuthState } from '@okta/okta-auth-js';
 import { filter, map, Observable } from 'rxjs';
 import Artwork from 'src/app/interfaces/artwork';
+import { LikeService } from 'src/app/services/like.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-gallery',
@@ -14,13 +16,17 @@ export class UserGalleryComponent implements OnInit {
   public isAuthenticated$!: Observable<boolean>;
   public likedArtworks!: Artwork[];
 
-  constructor(private _oktaStateService: OktaAuthStateService) { }
+  constructor(private _oktaStateService: OktaAuthStateService, private _likeService: LikeService, private _userService: UserService) { }
 
   ngOnInit(): void {
     this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
       filter((s: AuthState) => !!s),
       map((s: AuthState) => s.isAuthenticated ?? false)
     );
+
+    // Get the User from okta, from db, then get users liked posts
+    this._oktaStateService.authState$.subscribe(as => this._userService.getUserByEmail(as.accessToken?.claims.sub!)
+    .subscribe(u => this._likeService.getUsersLikes(u.id).subscribe(likes => this.likedArtworks = likes)));
   }
 
 }

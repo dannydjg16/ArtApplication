b1a67cd0ee7a8cd8762e297086a4e419c01c214a
dannydjg16 <dannydjg16@gmail.com>
2022-04-12 01:07:46 -0400

    Post Actions component filled out

--- src/app/components/post-actions/post-actions.component.ts
+++ src/app/components/post-actions/post-actions.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import Artwork from 'src/app/interfaces/artwork';
+import User from 'src/app/interfaces/user';
+import { LikeService } from 'src/app/services/like.service';
 
 @Component({
   selector: 'app-post-actions',
@@ -10,13 +13,20 @@ export class PostActionsComponent implements OnInit {
   @Input() theArtwork : any;
   @Input() theUser : any;
 
-  constructor() { }
+  constructor(private _likeService: LikeService) { }
 
   ngOnInit(): void {
   }
 
-  public LikeOrUnlikePost() {
-    
+  public LikePost(art: Artwork, user: User) {
+    this._likeService.likePost(art.id, user.id).subscribe(data => {
+      console.log(data);
+    });
   }
 
+  public UnlikePost(art: Artwork, user: User) {
+    this._likeService.likePost(art.id, user.id).subscribe(data => {
+      console.log(data);
+    });
+  }
 }

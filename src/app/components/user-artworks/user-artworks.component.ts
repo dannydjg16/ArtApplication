8eab802e645f1cb802124bcdab84253a39de8316
dannydjg16 <dannydjg16@gmail.com>
2022-04-20 23:52:28 -0400

    User Artwork component.ts class beginning

--- src/app/components/user-artworks/user-artworks.component.ts
+++ src/app/components/user-artworks/user-artworks.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import { AuthState } from '@okta/okta-auth-js';
+import { filter, map, Observable } from 'rxjs';
+import Artwork from 'src/app/interfaces/artwork';
+import { ArtworkService } from 'src/app/services/artwork.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-artworks',
@@ -7,9 +13,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserArtworksComponent implements OnInit {
 
-  constructor() { }
+  public isAuthenticated$!: Observable<boolean>;
+  public addedArtworks!: Artwork[];
+  @Input() theUser : any;
+
+  constructor(private _oktaStateService: OktaAuthStateService, 
+    private _artworkService: ArtworkService, 
+    private _userService: UserService) { }
 
   ngOnInit(): void {
+    this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
+      filter((s: AuthState) => !!s),
+      map((s: AuthState) => s.isAuthenticated ?? false)
+    );
+
+    // Get the User from okta, from db, then get users added artworks
+    
   }
 
 }

2d45ab8c7282393e96f03b76c65dde10d1ebd2a9
dannydjg16 <dannydjg16@gmail.com>
2022-04-20 23:48:32 -0400

    User Artwork Card component

---
+++ src/app/components/user-artwork-card/user-artwork-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-artwork-card',
+  templateUrl: './user-artwork-card.component.html',
+  styleUrls: ['./user-artwork-card.component.css']
+})
+export class UserArtworkCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

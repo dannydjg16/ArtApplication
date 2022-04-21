174b672a3d38c46bc24560278e430b8abd6350cc
dannydjg16 <dannydjg16@gmail.com>
2022-04-21 00:03:35 -0400

    UserArtwork Card

--- src/app/components/user-artwork-card/user-artwork-card.component.ts
+++ src/app/components/user-artwork-card/user-artwork-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-artwork-card',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-artwork-card.component.css']
 })
 export class UserArtworkCardComponent implements OnInit {
+
+  @Input() theArtwork : any;
+  @Input() theUser: any;
+  @Input() liked: any;
 
   constructor() { }
 

276773967789edf122422e9711179e06b86a03fe
dannydjg16 <dannydjg16@gmail.com>
2022-04-22 01:25:01 -0400

    Got all info for add art component

--- src/app/components/add-art/add-art.component.ts
+++ src/app/components/add-art/add-art.component.ts
@@ -3,9 +3,12 @@ import { OktaAuthStateService } from '@okta/okta-angular';
 import { AuthState } from '@okta/okta-auth-js';
 import { filter, map, Observable } from 'rxjs';
 import Artist from 'src/app/interfaces/artist';
+import ArtType from 'src/app/interfaces/arttype';
 import User from 'src/app/interfaces/user';
 import { ArtistService } from 'src/app/services/artist.service';
+import { ArttypeService } from 'src/app/services/arttype.service';
 import { ArtworkService } from 'src/app/services/artwork.service';
+import { LocationService } from 'src/app/services/location.service';
 import { UserService } from 'src/app/services/user.service';
 import  Artwork  from '../../interfaces/artwork';
 
@@ -19,11 +22,15 @@ export class AddArtComponent implements OnInit {
   public isAuthenticated$!: Observable<boolean>;
   public user!: User;
   public artists!: Artist[];
+  public artTypes!: ArtType[];
+  public locations!: Location[];
 
   constructor(private _oktaStateService: OktaAuthStateService, 
               private _artworkService: ArtworkService,
               private userService: UserService,
-              private _artistService: ArtistService) { }
+              private _artistService: ArtistService,
+              private _arttypeService: ArttypeService,
+              private _locationService: LocationService) { }
 
   ngOnInit(): void {
     this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
@@ -32,8 +39,9 @@ export class AddArtComponent implements OnInit {
     );
 
     this._oktaStateService.authState$.subscribe(as => this.userService.getUserByEmail(as.accessToken?.claims.sub!).subscribe(u => this.user = u));
-
     this._artistService.getArtists().subscribe(artists => this.artists = artists);
+    this._arttypeService.getArtTypes().subscribe(arttypes => this.artTypes = arttypes);
+    this._locationService.getLocations().subscribe(locations => this.locations = locations);
   }
 
   add(title: string, url: string, year: string, description: string, artist: string, medium: string, location: string, adder: number) {

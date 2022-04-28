eac824c87b59b686073506ce495b61c4688776f9
dannydjg16 <dannydjg16@gmail.com>
2022-04-27 23:54:17 -0400

    Get id from route

--- src/app/components/edit-artwork/edit-artwork.component.ts
+++ src/app/components/edit-artwork/edit-artwork.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { OktaAuthStateService } from '@okta/okta-angular';
 import User from 'src/app/interfaces/user';
 import { ArtworkService } from 'src/app/services/artwork.service';
@@ -13,17 +14,23 @@ export class EditArtworkComponent implements OnInit {
 
   public user!: User;
   public artPictureURL = "https://cdn.pixabay.com/photo/2014/08/25/16/17/picture-frame-427233_960_720.jpg";
-
+  sub: any;
 
   constructor(private location: Location, 
     private userService: UserService, 
     private _oktaStateService: OktaAuthStateService,
-    private _artworkService: ArtworkService) { }
+    private _artworkService: ArtworkService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this._oktaStateService.authState$.subscribe(as => this.userService.getUserByEmail(as.accessToken?.claims.sub!).subscribe(u => this.user =u));
 
     // Set Artwork Picture
+    this.sub = this.route.data.subscribe(id => console.log(id));
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }

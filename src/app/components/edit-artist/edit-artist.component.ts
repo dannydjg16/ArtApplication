5812149090a0247ee9609baa7fe80362146cb45b
dannydjg16 <dannydjg16@gmail.com>
2022-04-29 14:56:20 -0400

    Additions to Edit Artist

--- src/app/components/edit-artist/edit-artist.component.ts
+++ src/app/components/edit-artist/edit-artist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import { Observable } from 'rxjs';
+import { ArtistService } from 'src/app/services/artist.service';
 
 @Component({
   selector: 'app-edit-artist',
@@ -7,7 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditArtistComponent implements OnInit {
 
-  constructor() { }
+  public isAuthenticated$!: Observable<boolean>;
+  public artistPictureURL = "https://cdn.pixabay.com/photo/2014/08/25/16/17/picture-frame-427233_960_720.jpg";
+
+  constructor(private _oktaStateService: OktaAuthStateService, 
+    private _artistService: ArtistService) { }
 
   ngOnInit(): void {
   }

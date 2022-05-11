c830dd77db0178685e279614b116e763c4a3ba31
dannydjg16 <dannydjg16@gmail.com>
2022-05-11 06:51:32 -0400

    Add Location Types to Edit Location component

--- src/app/components/edit-location/edit-location.component.ts
+++ src/app/components/edit-location/edit-location.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { OktaAuthStateService } from '@okta/okta-angular';
 import Location from 'src/app/interfaces/location';
+import LocationType from 'src/app/interfaces/locationtype';
 import User from 'src/app/interfaces/user';
 import { LocationService } from 'src/app/services/location.service';
+import { LocationTypeService } from 'src/app/services/locationtype.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -15,15 +17,19 @@ export class EditLocationComponent implements OnInit {
   public user!: User;
   locaitonToEdit!: Location;
   route: any;
+  public locationtypes!: LocationType[];
+
 
   constructor(private userService: UserService,
               private _oktaStateService: OktaAuthStateService,
-              private _locationService: LocationService) { }
+              private _locationService: LocationService,
+              private _locationtypeService: LocationTypeService) { }
 
   ngOnInit(): void {
     this._oktaStateService.authState$.subscribe(as => this.userService.getUserByEmail(as.accessToken?.claims.sub!).subscribe(u => this.user = u));
 
     this._locationService.getLocationById(this.route.snapshot.params['id']).subscribe(loc => this.locaitonToEdit = loc);
+    this._locationtypeService.getLocationTypes().subscribe(loctyps => this.locationtypes = loctyps);
   }
 
   edit() {

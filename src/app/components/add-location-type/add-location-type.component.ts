d178a009f32016f9b3f399c60d3dbecb38a31eaa
dannydjg16 <dannydjg16@gmail.com>
2022-05-22 00:02:49 -0400

    Add Location Component ts file

--- src/app/components/add-location-type/add-location-type.component.ts
+++ src/app/components/add-location-type/add-location-type.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import { AuthState } from '@okta/okta-auth-js';
+import { filter, map, Observable } from 'rxjs';
+import LocationType from 'src/app/interfaces/locationtype';
+import User from 'src/app/interfaces/user';
+import { LocationTypeService } from 'src/app/services/locationtype.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-add-location-type',
@@ -7,9 +14,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddLocationTypeComponent implements OnInit {
 
-  constructor() { }
+  public user!: User;
+  public locationTypes!: LocationType[];
+
+  constructor(private _oktaStateService: OktaAuthStateService,
+              private _ltService: LocationTypeService,
+              private userService: UserService,) { }
 
   ngOnInit(): void {
+    this._oktaStateService.authState$
+      .subscribe(as => this.userService.getUserByEmail(as.accessToken?.claims.sub!)
+      .subscribe(u => this.user = u));
+  }
+
+  add(name: string) {
+
+    const locationType = {
+      name: name
+    }
+
+    this._ltService.addLocationType(locationType as unknown as LocationType).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }

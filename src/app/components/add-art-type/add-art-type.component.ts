43f8fa5415a4be6d3df4265203e6b94010492f63
dannydjg16 <dannydjg16@gmail.com>
2022-05-26 15:28:55 -0400

    Add Art TS file

--- src/app/components/add-art-type/add-art-type.component.ts
+++ src/app/components/add-art-type/add-art-type.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import ArtType from 'src/app/interfaces/arttype';
+import User from 'src/app/interfaces/user';
+import { ArttypeService } from 'src/app/services/arttype.service';
+import { LocationTypeService } from 'src/app/services/locationtype.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-add-art-type',
@@ -7,9 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddArtTypeComponent implements OnInit {
 
-  constructor() { }
+  public user!: User;
+
+  constructor(private _oktaStateService: OktaAuthStateService,
+              private _atService: ArttypeService,
+              private userService: UserService,) { }
 
   ngOnInit(): void {
+    this._oktaStateService.authState$
+    .subscribe(as => this.userService.getUserByEmail(as.accessToken?.claims.sub!)
+    .subscribe(u => this.user = u));
   }
 
+  add(name: string, description: string) {
+
+    const arttype = {
+      name: name,
+      description: description
+    }
+
+    this._atService.addArtType(arttype as unknown as ArtType).subscribe(data => {
+      console.log(data);
+    });
+  }
 }

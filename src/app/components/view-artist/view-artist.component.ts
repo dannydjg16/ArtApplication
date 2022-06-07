4e6ac7ee3f5a9a8a7da1e3965d72cba842220ef8
dannydjg16 <dannydjg16@gmail.com>
2022-06-07 19:28:14 -0400

    Can see Artist Name, from, picture

--- src/app/components/view-artist/view-artist.component.ts
+++ src/app/components/view-artist/view-artist.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import Artist from 'src/app/interfaces/artist';
+import User from 'src/app/interfaces/user';
+import { ArtistService } from 'src/app/services/artist.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-view-artist',
@@ -7,9 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewArtistComponent implements OnInit {
 
-  constructor() { }
+  public user!: User;
+  public artistPictureURL = "https://cdn.pixabay.com/photo/2014/08/25/16/17/picture-frame-427233_960_720.jpg";
+  public artist!: Artist;
+
+  constructor(private userService: UserService, 
+              private _oktaStateService: OktaAuthStateService,
+              private _artistService: ArtistService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this._oktaStateService.authState$.subscribe(as => this.userService.getUserByEmail(as.accessToken?.claims.sub!).subscribe(u => this.user = u));
+ 
+    this._artistService.getAnArtist(this.route.snapshot.params['id']).subscribe(artist => this.setArtistAndURL(artist));
   }
 
+
+  setArtistAndURL(artist: Artist) {
+    this.artist = artist;
+    this.artistPictureURL = artist.pictureURL;
+  }
 }

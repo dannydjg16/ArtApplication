b1db81f493ee1da5c07e1394c1963a112dc5b077
dannydjg16 <dannydjg16@gmail.com>
2022-06-08 17:16:56 -0400

    Artist Gallery Working

--- src/app/components/artist-works/artist-works.component.ts
+++ src/app/components/artist-works/artist-works.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import { AuthState } from '@okta/okta-auth-js';
+import { filter, map, Observable } from 'rxjs';
+import Artist from 'src/app/interfaces/artist';
+import Artwork from 'src/app/interfaces/artwork';
+import User from 'src/app/interfaces/user';
+import { ArtworkService } from 'src/app/services/artwork.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-artist-works',
@@ -7,9 +16,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistWorksComponent implements OnInit {
 
-  constructor() { }
+  public isAuthenticated$!: Observable<boolean>;
+  public artworkss!: Artwork[];
+  public signedInUser$!: User;
+  @Input() artist!: Artist;
+
+  constructor(private _oktaStateService: OktaAuthStateService, 
+              private _artworkService: ArtworkService, 
+              private _userService: UserService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
+      filter((s: AuthState) => !!s),
+      map((s: AuthState) => s.isAuthenticated ?? false)
+    );
+
+    // Getting the signed in user to pass to the gallery card to pass to the post action
+    this._oktaStateService.authState$.subscribe(as => this._userService.getUserByEmail(as.accessToken?.claims.sub!).subscribe(u => this.signedInUser$ = u));
+
+    this._artworkService.getArtworksByArtist(this.route.snapshot.params['id']).subscribe(aws => this.artworkss = aws);
   }
 
 }

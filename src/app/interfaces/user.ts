87584314d2d18390fc6d78db0ac66029b25fd308
dannydjg16 <dannydjg16@gmail.com>
2022-06-16 14:14:31 -0400

    Likes array added to User

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,7 +1,10 @@
+import Like from "./like";
+
 export default interface User{
     id: number;
     email: string;
     name: string;
     fromLocation: string;
     profilePicURL: string;
+    likes: Like[];
 }

c825fef562c1047891001d061158dc01a05af7d7
dannydjg16 <dannydjg16@gmail.com>
2022-06-23 01:36:32 -0400

    View Location shows location info
    Want to add the gallery now

--- src/app/components/view-location/view-location.component.ts
+++ src/app/components/view-location/view-location.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import Location from 'src/app/interfaces/location';
+import User from 'src/app/interfaces/user';
+import { LocationService } from 'src/app/services/location.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-view-location',
@@ -7,9 +13,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewLocationComponent implements OnInit {
 
-  constructor() { }
+  public user!: User;
+  public viewLocation!: Location;
+  public userAddedLocation: boolean = true;
+
+  constructor(private userService: UserService,
+              private _oktaStateService: OktaAuthStateService,
+              private _locationService: LocationService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this._oktaStateService.authState$
+    .subscribe(as => this.userService.getUserByEmail(as.accessToken?.claims.sub!)
+      .subscribe(u => this.setLocationAndUser(u)));
+  }
+
+  setLocationAndUser(user: User) {
+    this.user = user;
+    this._locationService.getLocationById(this.route.snapshot.params['id'])
+      .subscribe(location => this.viewLocation = location);
   }
 
 }

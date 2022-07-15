8b93b1b7cea2d5034561906171dd489f332ba21d
dannydjg16 <dannydjg16@gmail.com>
2022-07-15 14:56:24 -0400

    More transitioning of ABCing to back end

--- src/app/components/edit-artwork/edit-artwork.component.ts
+++ src/app/components/edit-artwork/edit-artwork.component.ts
@@ -12,7 +12,6 @@ import { ArtistService } from 'src/app/services/artist.service';
 import { ArttypeService } from 'src/app/services/arttype.service';
 import { LocationService } from 'src/app/services/location.service';
 
-
 @Component({
   selector: 'app-edit-artwork',
   templateUrl: './edit-artwork.component.html',

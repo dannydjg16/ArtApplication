9009d26065011b0a6a84ae14341f7849d5a39f4c
dannydjg16 <dannydjg16@gmail.com>
2022-07-30 23:58:08 -0400

    Let instead of var

--- src/app/components/Edit-Objects/edit-location/edit-location.component.ts
+++ src/app/components/Edit-Objects/edit-location/edit-location.component.ts
@@ -55,7 +55,7 @@ export class EditLocationComponent implements OnInit {
   }
 
   goBack() {
-    var id = this.route.snapshot.params['id'];
+    let id = this.route.snapshot.params['id'];
     this._router.navigate(['viewlocation' + '/'+ `${id}`]);
   }
 }

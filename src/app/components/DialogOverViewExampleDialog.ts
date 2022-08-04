0b12100265e73d0d71a58aee7715e1e2235a6a4d
dannydjg16 <dannydjg16@gmail.com>
2022-08-04 17:17:25 -0400

    Dialog first step to add.
    Could and would like to use this in multiple places

---
+++ src/app/components/DialogOverViewExampleDialog.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from "@angular/core";
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+export interface DialogData {
+    animal: string;
+    name: string;
+}
+
+@Component({
+    selector: 'dialog-overview-example-dialog',
+    templateUrl: 'dialog-overview-example-dialog.html',
+  })
+  export class DialogOverviewExampleDialog {
+  
+    constructor(
+      public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+      @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+  
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
+  
+  }

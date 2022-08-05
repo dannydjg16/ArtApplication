209eba9da7fb11228c5e38d1dd4c8e46bbe7d6bc
dannydjg16 <dannydjg16@gmail.com>
2022-08-04 23:29:02 -0400

    Got right imports in

--- src/app/components/DialogOverViewExampleDialog.ts
+++ src/app/components/DialogOverViewExampleDialog.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from "@angular/core";
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 export interface DialogData {
     animal: string;

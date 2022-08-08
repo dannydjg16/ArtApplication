db30a2e93d8a4d666d8279d1398d6944aa5704db
dannydjg16 <dannydjg16@gmail.com>
2022-08-08 00:01:32 -0400

    src code dialog

--- src/app/components/Dialog-Popup/DialogOverViewExampleDialog.ts
+++ src/app/components/Dialog-Popup/DialogOverViewExampleDialog.ts
@@ -1,23 +1,23 @@
-// import { Component, Inject } from "@angular/core";
-// import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, Inject } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
-// export interface DialogData {
-//     animal: string;
-//     name: string;
-// }
+export interface DialogData {
+    animal: string;
+    name: string;
+}
 
-// @Component({
-//     selector: 'dialog-overview-example-dialog',
-//     templateUrl: 'dialog-overview-example-dialog.html',
-//   })
-//   export class DialogOverviewExampleDialog {
+@Component({
+    selector: 'dialog-overview-example-dialog',
+    templateUrl: './DialogOverViewExampleDialog.html',
+  })
+  export class DialogOverviewExampleDialog {
   
-//     constructor(
-//       public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
-//       @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    constructor(
+      public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+      @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
   
-//     onNoClick(): void {
-//       this.dialogRef.close();
-//     }
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
   
-//   }
+  }
